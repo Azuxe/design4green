14c913faaf51cdf6dbbac985a16ae1917f2cd8a4
Raphaël Marzat <raphael.marzat@outlook.com>
2019-11-21 18:51:29 +0100

    Ajout de la création du tableau

--- public/assets/js/scriptUser.js
+++ public/assets/js/scriptUser.js
@@ -2,25 +2,58 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const entryElements = document.getElementsByClassName('ct-chart');
 
-    let arrayData = new Array(2);
+    let arrayData = new Array(3);
     
     arrayData[0] = [];
     arrayData[1] = [];
+    arrayData[2] = []
 
     for(let i = 0; i < entryElements.length; i++){
         arrayData[0][i] = entryElements[i].getAttribute("data-date");
         arrayData[1][i] = entryElements[i].getAttribute("data-conso");
+        arrayData[2][i] = i;
     }
+
+    //Displaying the chart
 
     Chartist.Line('.ct-chart', {
         labels: arrayData[0],
         series: [
-            arrayData[1],
-            Array(entryElements.length).keys()
+            arrayData[1]
         ]
-    }, {
-        width : '80%',
-        height: '500px'
     });
+
+    //Displaying the grid
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
+    let thDate = document.createElement("th");
+    let thDateContent = document.createTextNode("Date");
+    let thConso = document.createElement("th");
+    let thConsoContent = document.createTextNode("Consommation");
+
+    thConso.append(thConsoContent);
+    thDate.append(thDateContent);
+
+    tr.append(thDate);
+    tr.append(thConso);
+
+    table.append(tr);
+
+    for(let i = 0; i < arrayData[0].length; i++){
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+
+        let tdDate = document.createTextNode(arrayData[0][i]);
+        let tdConso = document.createTextNode(arrayData[1][i]);
+
+        td.append(tdDate);
+        td.append(tdConso);
+
+        tr.append(td);
+
+        table.append(tr);
+    }
+
+    document.getElementById("grapheConsommationMaison").appendChild(table);
 
 });
